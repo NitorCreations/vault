8179d710bdc4ca89a932d67420b1dea069853bf6
Pasi Niemi <pasi.niemi@nitorcreations.com>
2019-02-11 08:55:59 +0200

    IAM Policy fixes for describe stacks

--- python/setup.py
+++ python/setup.py
@@ -15,10 +15,10 @@ import sys
 from setuptools import setup
 
 setup(name='nitor-vault',
-      version='0.31',
+      version='0.32',
       description='Vault for storing locally encypted data in S3 using KMS keys',
       url='http://github.com/NitorCreations/vault',
-      download_url='https://github.com/NitorCreations/vault/tarball/0.31',
+      download_url='https://github.com/NitorCreations/vault/tarball/0.32',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
