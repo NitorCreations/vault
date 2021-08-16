5c51fd7339545a52eb31897cd4e870526820ee85
Pasi Niemi <pasi.niemi@nitorcreations.com>
2021-08-16 09:00:47 +0300

    Upgrade threadlocal-aws to include requests dependency

--- python/setup.py
+++ python/setup.py
@@ -34,7 +34,7 @@ setup(name='nitor-vault',
           'console_scripts': ['vault=n_vault.cli:main']
       },
       install_requires=[
-          'threadlocal-aws==0.9',
+          'threadlocal-aws==0.10',
           'requests',
           'argcomplete',
           'future',
