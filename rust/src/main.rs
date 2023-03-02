2e11ee55326072f2ae9117f72d414142732522f2
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-03-02 18:13:42 +0200

    add initial cli handling with `-a` and `-l` options

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,46 @@
+use std::error::Error;
+
 use nitor_vault::Vault;
 
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "Nitor Vault, see https://github.com/nitorcreations/vault for usage examples"
+)] // Reads info from `Cargo.toml`
+struct Args {
+    #[arg(short, long, help = "Print secret value for given name")]
+    lookup: Option<String>,
+
+    #[arg(short, long, help = "List available secrets")]
+    all: bool,
+}
+
 #[tokio::main]
-async fn main() {
-    println!("Nitor Vault, see https://github.com/nitorcreations/vault for usage examples");
-    println!("testing listing all vault items...");
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args: Args = parse_args().await;
+
     let client = Vault::new(None, None).await.expect("Error getting Vault");
-    list_all(&client).await;
-    // client
-    //     .store("testingtesting", "hello team".as_bytes())
-    //     .await;
-    // println!("{}", client.lookup("testingtesting").await.unwrap())
+
+    if let Some(name) = args.lookup.as_deref() {
+        println!("Loading value for: {name}");
+        println!("{}", client.lookup(name).await.unwrap());
+        return Ok(());
+    }
+
+    if args.all {
+        list_all(&client).await;
+        return Ok(());
+    }
+
+    Ok(())
+}
+
+async fn parse_args() -> Args {
+    Args::parse()
 }
 
 async fn list_all(vault: &Vault) {
