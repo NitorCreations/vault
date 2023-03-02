78fe52f8f392285251bc28c5665ede0b41ed104e
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2023-03-02 15:46:30 +0200

    change to a Result-based lib-implementation

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -32,19 +32,20 @@ struct Meta {
 }
 
 impl Vault {
-    pub async fn new(vault_stack: Option<&str>, region_opt: Option<&str>) -> Vault {
-        let region_provider = RegionProviderChain::first_try(region_opt.map(|r| Region::new(r.to_owned())))
-            .or_default_provider()
-            .or_else("eu-west-1");
+    pub async fn new(vault_stack: Option<&str>, region_opt: Option<&str>) -> Result<Vault, String> {
+        let region_provider =
+            RegionProviderChain::first_try(region_opt.map(|r| Region::new(r.to_owned())))
+                .or_default_provider()
+                .or_else("eu-west-1");
         let config = aws_config::from_env().region(region_provider).load().await;
-        let cf_params = get_cf_params(&config, vault_stack.unwrap_or("vault")).await;
-        Vault {
+        let cf_params = get_cf_params(&config, vault_stack.unwrap_or("vault")).await?;
+        Ok(Vault {
             region: config.region().unwrap().to_owned(),
             cf_params,
             s3: s3Client::new(&config),
             kms: kmsClient::new(&config),
             config,
-        }
+        })
     }
     pub fn test(&self) {
         println!(
@@ -53,37 +54,35 @@ impl Vault {
         );
     }
 
-    pub async fn list_all(&self) {
-        println!("{}", self.all().await.join("\n"));
-    }
-    async fn all(&self) -> Vec<String> {
-        let mut res: Vec<String> = Vec::new();
-
-        self.s3
+    pub async fn all(&self) -> Result<Vec<String>, String> {
+        let output = self
+            .s3
             .list_objects_v2()
             .bucket(&self.cf_params.bucket_name)
             .send()
             .await
-            .expect("failed to list buckets")
+            .map_err(|e| e.to_string())?;
+        Ok(output
             .contents()
-            .unwrap()
+            .ok_or("error getting S3 output contents")?
             .iter()
-            .for_each(|object| {
+            .filter_map(|object| {
                 if let Some(key) = object.key() {
-                    if key.ends_with(".aesgcm.encrypted") && !res.contains(&key[..key.len() - 17].to_owned()) {
-                        res.push(key[..key.len() - 17].to_owned());
-                    } else if key.ends_with(".encrypted") && !res.contains(&key[..key.len() - 10].to_owned()) {
-                        res.push(key[..key.len() - 10].to_owned());
+                    if key.ends_with(".aesgcm.encrypted") {
+                        Some(key[..key.len() - 17].to_owned())
+                    } else {
+                        None
                     }
+                } else {
+                    None
                 }
-            });
-        res
+            })
+            .collect())
     }
     pub async fn stack_info(&self) {
         println!("{:?}", get_cf_params(&self.config, "vault").await)
     }
-
-    async fn encrypt(&self, data: &[u8]) -> EncryptObject {
+    async fn encrypt(&self, data: &[u8]) -> Result<EncryptObject, String> {
         let key_dict = self
             .kms
             .generate_data_key()
@@ -91,18 +90,22 @@ impl Vault {
             .key_spec(DataKeySpec::Aes256)
             .send()
             .await
-            .expect("error getting key from KMS");
+            .map_err(|e| e.to_string())?;
+
         let plaintext = key_dict.plaintext().unwrap().as_ref();
-        let aesgcm_cipher: AesGcm<Aes256, typenum::U12> = AesGcm::new_from_slice(plaintext).unwrap();
+        let aesgcm_cipher: AesGcm<Aes256, typenum::U12> =
+            AesGcm::new_from_slice(plaintext).map_err(|e| e.to_string())?;
         let mut nonce: [u8; 12] = [0; 12];
         let mut rng = rand::thread_rng();
         rng.fill(nonce.as_mut_slice());
         let nonce = Nonce::from_slice(nonce.as_slice());
+
         let meta = serde_json::to_string(&Meta {
             alg: "AESGCM".to_owned(),
             nonce: general_purpose::STANDARD.encode(nonce),
         })
-        .unwrap();
+        .map_err(|e| e.to_string())?;
+
         let aes_gcm_ciphertext = aesgcm_cipher
             .encrypt(
                 nonce,
@@ -111,41 +114,48 @@ impl Vault {
                     aad: meta.as_bytes(),
                 },
             )
-            .unwrap()
-            .to_vec();
-        EncryptObject {
+            .map_err(|e| e.to_string())?;
+
+        Ok(EncryptObject {
             data_key: key_dict.ciphertext_blob().unwrap().to_owned().into_inner(),
             aes_gcm_ciphertext,
             meta,
-        }
+        })
     }
-    async fn get_s3_obj_as_vec(&self, key: String) -> Vec<u8> {
+
+    async fn get_s3_obj_as_vec(&self, key: String) -> Result<Vec<u8>, String> {
         self.s3
             .get_object()
             .bucket(self.cf_params.bucket_name.to_owned())
             .key(&key)
             .send()
             .await
-            .unwrap_or_else(|_| panic!("no such key:{}", key))
+            .map_err(|e| e.to_string())?
             .body
             .collect()
             .await
-            .unwrap()
-            .to_vec()
+            .map(|body| body.to_vec())
+            .map_err(|e| e.to_string())
     }
-    async fn direct_decrypt(&self, encrypted_data: &[u8]) -> Vec<u8> {
+    async fn direct_decrypt(&self, encrypted_data: &[u8]) -> Result<Vec<u8>, String> {
         self.kms
             .decrypt()
             .ciphertext_blob(Blob::new(encrypted_data))
             .send()
             .await
-            .unwrap()
+            .map_err(|e| e.to_string())?
             .plaintext()
-            .unwrap()
-            .to_owned()
-            .into_inner()
+            .map(|blob| blob.to_owned().into_inner())
+            .ok_or("Error parsing KMS plaintext".to_owned())
     }
-    async fn put_s3_obj(&self, body: aws_sdk_s3::types::ByteStream, key: &str) {
+    async fn put_s3_obj(
+        &self,
+        body: aws_sdk_s3::types::ByteStream,
+        key: &str,
+    ) -> Result<
+        aws_sdk_s3::output::PutObjectOutput,
+        aws_sdk_cloudformation::types::SdkError<aws_sdk_s3::error::PutObjectError>,
+    > {
         self.s3
             .put_object()
             .bucket(&self.cf_params.bucket_name)
@@ -154,32 +164,45 @@ impl Vault {
             .body(body)
             .send()
             .await
-            .expect("error saving key");
     }
-    pub async fn store(&self, name: &str, data: &[u8]) {
-        let encrypted = self.encrypt(data).await;
+    pub async fn store(&self, name: &str, data: &[u8]) -> Result<(), String> {
+        let encrypted = self.encrypt(data).await.map_err(|e| e.to_string())?;
         self.put_s3_obj(
             ByteStream::from(encrypted.aes_gcm_ciphertext),
             &format!("{}.aesgcm.encrypted", name),
         )
-        .await;
-        self.put_s3_obj(ByteStream::from(encrypted.data_key), &format!("{}.key", name))
-            .await;
+        .await
+        .map_err(|e| e.to_string())?;
+
+        self.put_s3_obj(
+            ByteStream::from(encrypted.data_key),
+            &format!("{}.key", name),
+        )
+        .await
+        .map_err(|e| e.to_string())?;
+
         self.put_s3_obj(
             ByteStream::from(encrypted.meta.as_bytes().to_owned()),
             &format!("{}.meta", name),
         )
-        .await;
+        .await
+        .map_err(|e| e.to_string())?;
+
+        Ok(())
     }
-    pub async fn lookup(&self, name: &str) -> String {
+    pub async fn lookup(&self, name: &str) -> Result<String, String> {
         let key = name;
-        let data_key = self.get_s3_obj_as_vec(format!("{key}.key")).await;
-        let meta_add = self.get_s3_obj_as_vec(format!("{key}.meta")).await;
-        let ciphertext = self.get_s3_obj_as_vec(format!("{key}.aesgcm.encrypted")).await;
-        let meta: Meta = serde_json::from_slice(&meta_add).unwrap();
+        let data_key = self.get_s3_obj_as_vec(format!("{key}.key")).await.unwrap();
+        let meta_add = self.get_s3_obj_as_vec(format!("{key}.meta")).await.unwrap();
+        let ciphertext = self
+            .get_s3_obj_as_vec(format!("{key}.aesgcm.encrypted"))
+            .await?;
+        let meta: Meta = serde_json::from_slice(&meta_add).map_err(|e| e.to_string())?;
         let cipher: AesGcm<Aes256, typenum::U12> =
-            AesGcm::new_from_slice(self.direct_decrypt(&data_key).await.as_slice()).unwrap();
-        let nonce = general_purpose::STANDARD.decode(meta.nonce).unwrap();
+            AesGcm::new_from_slice(self.direct_decrypt(&data_key).await?.as_slice()).unwrap();
+        let nonce = general_purpose::STANDARD
+            .decode(meta.nonce)
+            .map_err(|e| e.to_string())?;
         let nonce = Nonce::from_slice(nonce.as_slice());
         let res = cipher
             .decrypt(
@@ -189,8 +212,8 @@ impl Vault {
                     aad: &meta_add,
                 },
             )
-            .unwrap();
-        String::from_utf8(res).unwrap()
+            .map_err(|e| e.to_string())?;
+        Ok(String::from_utf8(res).unwrap())
     }
 }
 
@@ -206,25 +229,23 @@ fn parse_output_value_from_key(key: &str, out: &[Output]) -> Option<String> {
         .map(|output| output.output_value().unwrap_or_default().to_owned())
 }
 
-async fn get_cf_params(config: &SdkConfig, stack: &str) -> CfParams {
+async fn get_cf_params(config: &SdkConfig, stack: &str) -> Result<CfParams, String> {
     let stack_output = cfClient::new(config)
         .describe_stacks()
         .stack_name(stack)
         .send()
         .await
-        .expect("error getting stack information")
+        .map_err(|err| err.to_string())?
         .stacks()
-        .unwrap_or_default()
-        .first()
-        .expect("stack not found")
-        .outputs()
-        .unwrap()
+        .and_then(|stacks| stacks.first())
+        .and_then(|stack| stack.outputs())
+        .ok_or("error getting Cloudformation Stack Ouput")?
         .to_owned();
 
-    CfParams {
+    Ok(CfParams {
         bucket_name: parse_output_value_from_key("vaultBucketName", &stack_output)
-            .expect("Error getting bucket name from stack"),
+            .ok_or("Error getting bucket name from stack")?,
         key_arn: parse_output_value_from_key("kmsKeyArn", &stack_output),
         // deployed_version: parse_output_value_from_key("vaultStackVersion", &stack_output),
-    }
+    })
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,10 +2,17 @@ use vault::Vault;
 
 #[tokio::main]
 async fn main() {
-    let client = Vault::new(None, None).await;
-    // client.list_all().await;
+    let client = Vault::new(None, None).await.expect("Error getting Vault");
+    list_all(&client).await;
     // client
     //     .store("testingtesting", "hello team".as_bytes())
     //     .await;
-    println!("{}", client.lookup("testingtesting").await)
+    println!("{}", client.lookup("testingtesting").await.unwrap())
+}
+
+async fn list_all(vault: &Vault) {
+    match vault.all().await {
+        Ok(all) => println!("{}", all.join("\n")),
+        Err(error) => println!("error occurred: {}", error),
+    }
 }
