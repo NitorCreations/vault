5e7edbee8b3ad68612b0b7331c085ee513c71b7a
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2023-03-02 15:50:26 +0200

    initial release

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,15 @@
-use vault::Vault;
+use nitor_vault::Vault;
 
 #[tokio::main]
 async fn main() {
+    println!("Nitor Vault, see https://github.com/nitorcreations/vault for usage examples");
+    println!("testing listing all vault items...");
     let client = Vault::new(None, None).await.expect("Error getting Vault");
     list_all(&client).await;
     // client
     //     .store("testingtesting", "hello team".as_bytes())
     //     .await;
-    println!("{}", client.lookup("testingtesting").await.unwrap())
+    // println!("{}", client.lookup("testingtesting").await.unwrap())
 }
 
 async fn list_all(vault: &Vault) {
