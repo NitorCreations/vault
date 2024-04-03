bdec4621502c2deef8c32d44aeb0d995609a3e6f
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-04-03 11:11:12 +0300

    Rust lib improvements (#391)
    * library improvements
    
    * use new
    
    * revert leftover writeln usage
    
    * delete objects with one call
    
    * add docstrings and improve error message
    
    * rename method to follow naming conventions
    
    * implement `new` for meta and make json a method
    
    * move cloudformation methods to `CloudFormationParams`
    
    * return result from `to_object_identifiers`
    
    * rename variable
    
    * revert move to associated functions
    
    * cargo update

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> Result<()> {
     let args: Args = cli::parse_args();
 
     // Bucket and key were either given as args or found in env variables
-    let client = if args.bucket.is_some() && args.key_arn.is_some() {
+    let vault = if args.bucket.is_some() && args.key_arn.is_some() {
         Vault::from_params(
             CloudFormationParams::from(args.bucket.as_deref().unwrap(), args.key_arn.as_deref()),
             args.region.as_deref(),
@@ -30,19 +30,19 @@ async fn main() -> Result<()> {
     // Handle subcommands
     if let Some(command) = args.command {
         return match command {
-            Command::Delete { key } => cli::delete(&client, &key).await,
-            Command::Describe {} => Ok(println!("{}", client.stack_info())),
-            Command::Exists { key } => cli::exists(&client, &key).await,
-            Command::All {} => cli::list_all(&client).await,
-            Command::Lookup { key } => cli::lookup(&client, &key).await,
+            Command::Delete { key } => cli::delete(&vault, &key).await,
+            Command::Describe {} => Ok(println!("{}", vault.stack_info())),
+            Command::Exists { key } => cli::exists(&vault, &key).await,
+            Command::All {} => cli::list_all(&vault).await,
+            Command::Lookup { key } => cli::lookup(&vault, &key).await,
             Command::Store {
                 key,
                 value,
                 overwrite,
                 file,
                 value_opt,
-            } => cli::store(&client, key, value, file, value_opt, overwrite).await,
-            Command::Info {} => Ok(println!("{}", client)),
+            } => cli::store(&vault, key, value, file, value_opt, overwrite).await,
+            Command::Info {} => Ok(println!("{}", vault)),
         };
     }
     Ok(())
