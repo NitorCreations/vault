e25a9cb06c4323a561b04c65c3d345deaf13e364
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2024-04-20 02:04:21 +0300

    node release 1.2.0
    CLI: don't print newline after lookup result
    
    Add VAULT_STACK env variable to determine CloudFormation stack name

--- nodejs/cli/vault.ts
+++ nodejs/cli/vault.ts
@@ -7,7 +7,7 @@ const handleRejection = (err: string) => {
   process.exit(1);
 };
 
-const prog = sade("vault").version("1.1.1");
+const prog = sade("vault").version("1.2.0");
 
 prog.option(
   "--vaultstack",
