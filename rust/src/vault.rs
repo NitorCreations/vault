8dea21f2e39109b0b8ed29b003001e94a80e5c13
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-10-08 00:17:08 +0300

    rust: implement prefix option and refactor vault factory methods

--- rust/src/vault.rs
+++ rust/src/vault.rs
@@ -25,18 +25,34 @@ pub struct Vault {
     /// AWS region to use with Vault.
     /// Will fall back to default provider if nothing is specified.
     region: Region,
+    /// Prefix for key name
+    prefix: String,
     cloudformation_params: CloudFormationParams,
     s3: S3Client,
     kms: KmsClient,
 }
 
 impl Vault {
+    /// Construct Vault with defaults for an existing stack.
+    /// This will try reading environment variables for the config values,
+    /// and otherwise fall back to current AWS config.
+    ///
+    /// The Default trait can't be implemented for Vault since it can fail.
+    pub async fn default() -> Result<Self, VaultError> {
+        Self::new(None, None, None, None, None).await
+    }
+
+    /// Construct Vault with optional arguments for an existing stack.
+    /// This will try reading environment variables for the config values if they are `None`.
     pub async fn new(
-        vault_stack: Option<&str>,
-        region_opt: Option<&str>,
+        vault_stack: Option<String>,
+        region: Option<String>,
+        bucket: Option<String>,
+        key: Option<String>,
+        prefix: Option<String>,
     ) -> Result<Self, VaultError> {
         let config = aws_config::from_env()
-            .region(get_region_provider(region_opt))
+            .region(get_region_provider(region))
             .load()
             .await;
 
@@ -47,48 +63,28 @@ impl Vault {
 
         // Check env variables directly in case the library is not used through the CLI.
         // These are also handled in the CLI, so they are documented in the CLI help.
-        let vault_stack_from_env = get_env_variable("VAULT_STACK");
-        let vault_bucket_from_env = get_env_variable("VAULT_BUCKET");
-        let vault_key_from_env = get_env_variable("VAULT_KEY");
+        let stack_name = vault_stack
+            .or_else(|| get_env_variable("VAULT_STACK"))
+            .unwrap_or_else(|| "vault".to_string());
+        let bucket = bucket.or_else(|| get_env_variable("VAULT_BUCKET"));
+        let key = key.or_else(|| get_env_variable("VAULT_KEY"));
+        let mut prefix = prefix
+            .or_else(|| get_env_variable("VAULT_PREFIX"))
+            .unwrap_or_default();
 
-        let cloudformation_params =
-            if let (Some(bucket), Some(key)) = (vault_bucket_from_env, vault_key_from_env) {
-                CloudFormationParams::new(bucket, Some(key))
-            } else {
-                let stack_name = vault_stack_from_env
-                    .as_deref()
-                    .or(vault_stack)
-                    .unwrap_or("vault");
+        if !prefix.is_empty() && !prefix.ends_with('/') {
+            prefix.push('/');
+        }
 
-                CloudFormationParams::get_from_stack(&config, stack_name).await?
-            };
+        let cloudformation_params = if let (Some(bucket), Some(key)) = (bucket, key) {
+            CloudFormationParams::new(bucket, Some(key))
+        } else {
+            CloudFormationParams::from_stack(&config, stack_name).await?
+        };
 
         Ok(Self {
             region,
-            cloudformation_params,
-            s3: S3Client::new(&config),
-            kms: KmsClient::new(&config),
-        })
-    }
-
-    pub async fn from_cli_params(
-        bucket: &str,
-        key_arn: Option<&str>,
-        region_opt: Option<&str>,
-    ) -> Result<Self, VaultError> {
-        Self::from_params(CloudFormationParams::from(bucket, key_arn), region_opt).await
-    }
-
-    pub async fn from_params(
-        cloudformation_params: CloudFormationParams,
-        region_opt: Option<&str>,
-    ) -> Result<Self, VaultError> {
-        let config = aws_config::from_env()
-            .region(get_region_provider(region_opt))
-            .load()
-            .await;
-        Ok(Self {
-            region: config.region().ok_or(VaultError::NoRegionError)?.to_owned(),
+            prefix,
             cloudformation_params,
             s3: S3Client::new(&config),
             kms: KmsClient::new(&config),
@@ -124,6 +120,126 @@ impl Vault {
     #[must_use]
     pub fn stack_info(&self) -> CloudFormationParams {
         self.cloudformation_params.clone()
+    }
+
+    /// Check if key already exists in bucket
+    pub async fn exists(&self, name: &str) -> Result<bool, VaultError> {
+        let name = self.full_key_name(name);
+        match self
+            .s3
+            .head_object()
+            .bucket(self.cloudformation_params.bucket_name.clone())
+            .key(format!("{name}.key"))
+            .send()
+            .await
+        {
+            Ok(_) => Ok(true),
+            Err(e) => {
+                let service_error = e.into_service_error();
+                if service_error.is_not_found() {
+                    // The object does not exist
+                    Ok(false)
+                } else {
+                    // Propagate other errors like networking or permissions
+                    Err(VaultError::S3HeadObjectError(service_error))
+                }
+            }
+        }
+    }
+
+    /// Store encrypted data in S3
+    pub async fn store(&self, name: &str, data: &[u8]) -> Result<(), VaultError> {
+        let encrypted = self.encrypt(data).await?;
+
+        let key = &self.full_key_name(name);
+        let keys = S3DataKeys::new(key);
+
+        let put_cipher =
+            self.put_s3_object(keys.cipher, ByteStream::from(encrypted.aes_gcm_ciphertext));
+        let put_key = self.put_s3_object(keys.key, ByteStream::from(encrypted.data_key));
+        let put_meta = self.put_s3_object(keys.meta, ByteStream::from(encrypted.meta.into_bytes()));
+
+        try_join!(put_cipher, put_key, put_meta)?;
+
+        Ok(())
+    }
+
+    /// Delete data in S3 for given key
+    pub async fn delete(&self, name: &str) -> Result<(), VaultError> {
+        let key = &self.full_key_name(name);
+        if !self.exists(key).await? {
+            return Err(VaultError::S3DeleteObjectKeyMissingError);
+        }
+
+        let identifiers = S3DataKeys::new(key).to_object_identifiers()?;
+        self.s3
+            .delete_objects()
+            .bucket(&self.cloudformation_params.bucket_name)
+            .delete(Delete::builder().set_objects(Some(identifiers)).build()?)
+            .send()
+            .await?;
+
+        Ok(())
+    }
+
+    /// Return value for the given key name.
+    /// If the data is valid UTF-8, it will be returned as a string.
+    /// Otherwise, the raw bytes will be returned.
+    pub async fn lookup(&self, name: &str) -> Result<Value, VaultError> {
+        let key = &self.full_key_name(name);
+        let keys = S3DataKeys::new(key);
+
+        let data_key = self.get_s3_object(keys.key);
+        let cipher_text = self.get_s3_object(keys.cipher);
+        let meta_add = self.get_s3_object(keys.meta);
+        let (data_key, cipher_text, meta_add) = try_join!(data_key, cipher_text, meta_add)?;
+
+        let meta: Meta = serde_json::from_slice(&meta_add)?;
+        let cipher: AesGcm<Aes256, cipher::typenum::U12> =
+            AesGcm::new_from_slice(self.direct_decrypt(&data_key).await?.as_slice())?;
+        let nonce = base64::engine::general_purpose::STANDARD.decode(meta.nonce)?;
+        let nonce = Nonce::from_slice(nonce.as_slice());
+        let decrypted_bytes = cipher
+            .decrypt(
+                nonce,
+                Payload {
+                    msg: &cipher_text,
+                    aad: &meta_add,
+                },
+            )
+            .map_err(|_| VaultError::NonceDecryptError)?;
+
+        match String::from_utf8(decrypted_bytes) {
+            Ok(valid_string) => Ok(Value::Utf8(valid_string)),
+            Err(from_utf8_error) => Ok(Value::Binary(from_utf8_error.into_bytes())),
+        }
+    }
+
+    /// Get S3 Object data for given key as a vec of bytes
+    async fn get_s3_object(&self, key: String) -> Result<Vec<u8>, VaultError> {
+        self.s3
+            .get_object()
+            .bucket(self.cloudformation_params.bucket_name.clone())
+            .key(&key)
+            .send()
+            .await?
+            .body
+            .collect()
+            .await
+            .map_err(|_| VaultError::S3GetObjectBodyError)
+            .map(aws_sdk_s3::primitives::AggregatedBytes::to_vec)
+    }
+
+    /// Get decrypted data
+    async fn direct_decrypt(&self, encrypted_data: &[u8]) -> Result<Vec<u8>, VaultError> {
+        self.kms
+            .decrypt()
+            .ciphertext_blob(Blob::new(encrypted_data))
+            .send()
+            .await?
+            .plaintext()
+            .map(|blob| blob.to_owned().into_inner())
+            .ok_or(VaultError::KMSDataKeyPlainTextMissingError)
     }
 
     /// Encrypt data
@@ -173,38 +289,11 @@ impl Vault {
         })
     }
 
-    /// Get S3 Object data for given key as a vec of bytes
-    async fn get_s3_object(&self, key: String) -> Result<Vec<u8>, VaultError> {
-        self.s3
-            .get_object()
-            .bucket(self.cloudformation_params.bucket_name.clone())
-            .key(&key)
-            .send()
-            .await?
-            .body
-            .collect()
-            .await
-            .map_err(|_| VaultError::S3GetObjectBodyError)
-            .map(aws_sdk_s3::primitives::AggregatedBytes::to_vec)
-    }
-
-    /// Get decrypted data
-    async fn direct_decrypt(&self, encrypted_data: &[u8]) -> Result<Vec<u8>, VaultError> {
-        self.kms
-            .decrypt()
-            .ciphertext_blob(Blob::new(encrypted_data))
-            .send()
-            .await?
-            .plaintext()
-            .map(|blob| blob.to_owned().into_inner())
-            .ok_or(VaultError::KMSDataKeyPlainTextMissingError)
-    }
-
     /// Send PUT request with the given byte data
     async fn put_s3_object(
         &self,
-        body: ByteStream,
         key: String,
+        body: ByteStream,
     ) -> Result<PutObjectOutput, VaultError> {
         Ok(self
             .s3
@@ -217,94 +306,19 @@ impl Vault {
             .await?)
     }
 
-    /// Check if key already exists in bucket
-    pub async fn exists(&self, name: &str) -> Result<bool, VaultError> {
-        match self
-            .s3
-            .head_object()
-            .bucket(self.cloudformation_params.bucket_name.clone())
-            .key(format!("{name}.key"))
-            .send()
-            .await
-        {
-            Ok(_) => Ok(true),
-            Err(e) => {
-                let service_error = e.into_service_error();
-                if service_error.is_not_found() {
-                    // The object does not exist
-                    Ok(false)
-                } else {
-                    // Propagate other errors like networking or permissions
-                    Err(VaultError::S3HeadObjectError(service_error))
-                }
-            }
+    /// Add prefix to key if prefix has been specified.
+    fn full_key_name(&self, name: &str) -> String {
+        if self.prefix.is_empty() {
+            name.to_string()
+        } else {
+            format!("{}{}", self.prefix, name)
         }
     }
+}
 
-    /// Store encrypted data in S3
-    pub async fn store(&self, name: &str, data: &[u8]) -> Result<(), VaultError> {
-        let encrypted = self.encrypt(data).await?;
-        let first = self.put_s3_object(
-            ByteStream::from(encrypted.aes_gcm_ciphertext),
-            format!("{name}.aesgcm.encrypted"),
-        );
-        let second =
-            self.put_s3_object(ByteStream::from(encrypted.data_key), format!("{name}.key"));
-        let third = self.put_s3_object(
-            ByteStream::from(encrypted.meta.into_bytes()),
-            format!("{name}.meta"),
-        );
-        try_join!(first, second, third)?;
-
-        Ok(())
-    }
-
-    /// Delete data in S3 for given key
-    pub async fn delete(&self, name: &str) -> Result<(), VaultError> {
-        if !self.exists(name).await? {
-            return Err(VaultError::S3DeleteObjectKeyMissingError);
-        }
-
-        let keys = S3DataKeys::new(name);
-        let identifiers = keys.to_object_identifiers()?;
-        self.s3
-            .delete_objects()
-            .bucket(&self.cloudformation_params.bucket_name)
-            .delete(Delete::builder().set_objects(Some(identifiers)).build()?)
-            .send()
-            .await?;
-
-        Ok(())
-    }
-
-    /// Return value for the given key name.
-    /// If the data is valid UTF-8, it will be returned as a string.
-    /// Otherwise, the raw bytes will be returned.
-    pub async fn lookup(&self, name: &str) -> Result<Value, VaultError> {
-        let keys = S3DataKeys::new(name);
-        let data_key = self.get_s3_object(keys.key);
-        let cipher_text = self.get_s3_object(keys.cipher);
-        let meta_add = self.get_s3_object(keys.meta);
-        let (data_key, cipher_text, meta_add) = try_join!(data_key, cipher_text, meta_add)?;
-        let meta: Meta = serde_json::from_slice(&meta_add)?;
-        let cipher: AesGcm<Aes256, cipher::typenum::U12> =
-            AesGcm::new_from_slice(self.direct_decrypt(&data_key).await?.as_slice())?;
-        let nonce = base64::engine::general_purpose::STANDARD.decode(meta.nonce)?;
-        let nonce = Nonce::from_slice(nonce.as_slice());
-        let res = cipher
-            .decrypt(
-                nonce,
-                Payload {
-                    msg: &cipher_text,
-                    aad: &meta_add,
-                },
-            )
-            .map_err(|_| VaultError::NonceDecryptError)?;
-
-        match String::from_utf8(res) {
-            Ok(valid_string) => Ok(Value::Utf8(valid_string)),
-            Err(from_utf8_error) => Ok(Value::Binary(from_utf8_error.into_bytes())),
-        }
+impl fmt::Display for Vault {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "region: {}\n{}", self.region, self.cloudformation_params)
     }
 }
 
@@ -316,17 +330,11 @@ fn create_random_nonce() -> [u8; 12] {
 }
 
 /// Get AWS region from optional argument or fallback to default
-fn get_region_provider(region: Option<&str>) -> RegionProviderChain {
-    RegionProviderChain::first_try(region.map(|r| Region::new(r.to_owned()))).or_default_provider()
+fn get_region_provider(region: Option<String>) -> RegionProviderChain {
+    RegionProviderChain::first_try(region.map(Region::new)).or_default_provider()
 }
 
 /// Return possible env variable value as Option
 fn get_env_variable(name: &str) -> Option<String> {
     env::var(name).ok()
-}
-
-impl fmt::Display for Vault {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "region: {}\n{}", self.region, self.cloudformation_params)
-    }
 }
