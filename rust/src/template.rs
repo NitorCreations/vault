15eba4ae80b3c0ed3ae5104253aad4b517e6406b
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-10-16 18:14:51 +0300

    update python code

--- rust/src/template.rs
+++ rust/src/template.rs
@@ -337,28 +337,28 @@ static TEMPLATE_STRING: LazyLock<String> = LazyLock::new(|| {
             "Fn::Join": [
               "\n",
               [
-                "import json",
-                "import logging",
-                "import boto3",
                 "import base64",
+                "import boto3",
                 "import cfnresponse",
+                "import logging",
                 "log = logging.getLogger()",
                 "log.setLevel(logging.INFO)",
                 "kms = boto3.client('kms')",
                 "SUCCESS = 'SUCCESS'",
                 "FAILED = 'FAILED'",
                 "def handler(event, context):",
-                "  ciphertext = event['ResourceProperties']['Ciphertext']",
-                "  responseData = {}",
-                "  try:",
-                "    responseData['Plaintext'] = kms.decrypt(CiphertextBlob=base64.b64decode(ciphertext)).get('Plaintext').decode()",
-                "    log.info('Decrypt successful!')",
-                "    cfnresponse.send(event, context, SUCCESS, responseData, event['LogicalResourceId'])",
-                "  except Exception as e:",
-                "    error_msg = 'Failed to decrypt: ' + repr(e)",
-                "    log.error(error_msg)",
-                "    cfnresponse.send(event, context, FAILED, responseData, event['LogicalResourceId'])",
-                "    raise Exception(error_msg)"
+                "    ciphertext = event['ResourceProperties']['Ciphertext']",
+                "    resource_id = event.get('LogicalResourceId')",
+                "    response_data = {}",
+                "    try:",
+                "        response_data['Plaintext'] = kms.decrypt(CiphertextBlob=base64.b64decode(ciphertext)).get('Plaintext').decode()",
+                "        log.info('Decrypt successful!')",
+                "        cfnresponse.send(event, context, SUCCESS, response_data, resource_id)",
+                "    except Exception as e:",
+                "        error_msg = 'Failed to decrypt: ' + repr(e)",
+                "        log.error(error_msg)",
+                "        cfnresponse.send(event, context, FAILED, response_data, resource_id)",
+                "        raise Exception(error_msg)"
               ]
             ]
           }
